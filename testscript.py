60a4db27ea18764b750df52b33a3d9700a80fd57
teryzwiak <patryk.jozwiak2001@gmail.com>
2024-01-24 12:28:40 +0000

    test

--- testscript.py
+++ testscript.py
@@ -1,12 +1,23 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import re
 
 url = "https://eskarbonka.wosp.org.pl/gikygofudy?fbclid=IwAR3OoVjyApMU8eTr-VjMfJlbezY2v5qy-Ng0HpOliGpVThqS2ew9QX5aKPo"
 
-page = urlopen(url)
+#page = urlopen(url)
 
-page
+#page
 
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+#html_bytes = page.read()
+#html = html_bytes.decode("utf-8")
 
-print(html)
+#print(html)
+
+req = requests.get('https://eskarbonka.wosp.org.pl/gikygofudy?fbclid=IwAR3OoVjyApMU8eTr-VjMfJlbezY2v5qy-Ng0HpOliGpVThqS2ew9QX5aKPo')
+req
+soup = BeautifulSoup(req.content, "html.parser")
+start = soup.find('div')
+start = start.find("price")
+re.find()
+print(start)
